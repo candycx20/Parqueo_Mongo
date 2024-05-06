ee4b989fe332f2d7c644bcbc9b8437d32386329f
candycx20 <candycarrillo060@gmail.com>
2024-05-05 19:13:13 -0600

    Respusta de estados

--- controllers/TicketController.js
+++ controllers/TicketController.js
@@ -1,9 +1,6 @@
-//importamos el Modelo
 import TicketModel from "../models/TicketModel.js";
 
-//** Métodos para el CRUD **/
 
-//Mostrar todos los registros
 export const getAllTickets = async (req, res) => {
     try {
         const tickets = await TicketModel.find()
@@ -12,14 +9,19 @@ export const getAllTickets = async (req, res) => {
         res.json( {message: error.message} )
     }
 }
-//Crear un registro
+
 export const createTicket = async (req, res) => {
+    const estado = 0;
     try {
        await TicketModel.create(req.body)
        res.json({
+            estado: 1,
            "message":"¡Registro creado correctamente!"
        })
     } catch (error) {
-        res.json( {message: error.message} )
+        res.json( {
+            estado: 2,
+            message: error.message
+        } )
     }
 }
